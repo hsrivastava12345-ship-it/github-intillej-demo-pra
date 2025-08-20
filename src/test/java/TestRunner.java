8c81fff4924171aff6979ffbf1c7cfc3dc49566e
harshitsrivastava <h.srivastava12345@gmail.com>
2025-08-20 23:34:49 +0530

    Commiting Target Folder

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,7 +6,11 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
         features = "src/test/features", // Path to your feature files
         glue = {"Stepdefination"},              // Package with step defs
-        plugin = {"pretty", "html:target/cucumber-reports.html"}, // Reports
+        plugin = {"pretty",
+                "html:target/cucumber-reports/html-report.html",   // HTML report
+                "json:target/cucumber-reports/report.json",        // JSON report
+                "junit:target/cucumber-reports/report.xml"         // JUnit XML
+        },
         monochrome = true,                       // Cleaner console output
         tags = "@Regression"                      // Run specific tagged scenarios
 )
