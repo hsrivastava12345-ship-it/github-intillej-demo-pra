bb35061790933b5cc137851ed88f3d56766bd1d5
harshitsrivastava <h.srivastava12345@gmail.com>
2025-08-24 11:07:59 +0530

    commit

--- src/test/Features/First.feature
+++ src/test/Features/First.feature
@@ -4,6 +4,7 @@ Feature: Selenium Fundamentals
   @Regression
   Scenario Outline: Successfully Testing the Blogspot Website
     Given Go to the website "<Website>"
+    Then select country in input feild
 
     Examples:
       | Website                                      |
--- src/test/java/Stepdefination/mystepdefs.java
+++ src/test/java/Stepdefination/mystepdefs.java
@@ -1,6 +1,7 @@
 package Stepdefination;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,12 +10,20 @@ import org.testng.Assert;
 
 public class mystepdefs {
 
+    public static WebDriver driver;
+
     @Given("Go to the website {string}")
     public void goToTheWebsite(String arg0) {
 
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(arg0);
 
+    }
+
+    @Then("select country in input feild")
+    public void selectCountryInInputFeild() {
+        Select select = new Select(driver.findElement(By.xpath("//select[@id='country']")));
+        select.selectByContainsVisibleText("Australia");
     }
 }
